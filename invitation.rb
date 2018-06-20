128367475032422620916e3d88b076e5f06eb022
cassiebeisheim <cassiebeisheim@gmail.com>
2018-06-20 14:05:13 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -2,14 +2,14 @@
 
 # Try starting out with puts'ing a string.
 
-# guest_name="Hermoine"
-# party_name="the Best Halloween Party Ever"
-# date="October 31"
+guest_name="Hermoine"
+party_name="the Best Halloween Party Ever"
+date="October 31"
 # time="6pm"
 # rsvp_date="October 30"
 # host_name="Harry Potter"
 
-def invitation
+def invitation(guest_name, party_name, date, time, rsvp_date, host_name)
 
 puts "Dear #{guest_name},
 
